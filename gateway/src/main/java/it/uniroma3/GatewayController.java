c35e303ddd5db003d29660b78b649bcfc6181f04
ionchi <ion.chiriac@live.com>
2017-06-03 19:03:03 +0200

    html improvements

--- gateway/src/main/java/it/uniroma3/GatewayController.java
+++ gateway/src/main/java/it/uniroma3/GatewayController.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
@@ -21,7 +23,7 @@ public class GatewayController {
         return "index";
     }
 
-    @RequestMapping(value = "/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String getMovie(@PathVariable Long id, Model model) {
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -38,7 +40,7 @@ public class GatewayController {
         }
     }
 
-    @RequestMapping(value = "/movies")
+    @RequestMapping(value = "/movies", method = RequestMethod.GET)
     public String getAllMovies(Model model) {
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -54,6 +56,16 @@ public class GatewayController {
         } finally {
             return "movieList";
         }
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String getAllMovies(@PathVariable Long id, Model model) throws Exception {
+        URL url = new URL("http://localhost:8080/movie/"+id);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("DELETE");
+        connection.getResponseCode();
+        connection.disconnect();
+        return this.getAllMovies(model);
     }
 
 }
